6589d36c365dcca84ef6db538cbcf688010043ef
Dard001 <dard001@hotmail.com>
2020-07-13 16:45:19 -0600

    7/13/2020 Commit

---
+++ src/com/gcomputers/pattern/creational/singleton/LazySingleton.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2020 Dard
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.gcomputers.pattern.creational.singleton;
+
+/**
+ *
+ * @author Dard
+ */
+public class LazySingleton {
+    private static LazySingleton instance;
+    
+    public static LazySingleton getInstance(){
+        if (instance == null){
+            instance = new LazySingleton();
+        }
+        return instance;
+    }
+    
+    private LazySingleton(){
+        
+    }
+}
