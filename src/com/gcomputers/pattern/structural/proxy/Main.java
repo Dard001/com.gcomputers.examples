3d4d45a9660323da3f9062d211b37f87ee647b91
Dard001 <dard001@hotmail.com>
2020-07-13 18:53:46 -0600

    7/13/2020 Commit

--- src/com/gcomputers/pattern/structural/proxy/Main.java
+++ src/com/gcomputers/pattern/structural/proxy/Main.java
@@ -24,6 +24,5 @@ public class Main {
     public static void main(String[] args){
         ExpensiveObject object = new ExpensiveObjectProxy();
         object.process();
-        object.process();
     }
 }
