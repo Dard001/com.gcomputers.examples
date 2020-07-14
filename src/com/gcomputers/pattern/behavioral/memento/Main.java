2be6d80bf09619d6af4d51534c21437c7f3de306
Dard001 <dard001@hotmail.com>
2020-07-14 09:37:47 -0600

    7/14/2020 Commit

---
+++ src/com/gcomputers/pattern/behavioral/memento/Main.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2020 Dard
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.gcomputers.pattern.behavioral.memento;
+
+/**
+ *
+ * @author Dard
+ */
+public class Main {
+    public static void main(String[] args){
+        Originator originator = new Originator();
+        CareTaker careTaker = new CareTaker();
+        
+        originator.setState("State 1");
+        originator.setState("State 2");
+        careTaker.add(originator.saveStateToMemento());
+        
+        originator.setState("State 3");
+        careTaker.add(originator.saveStateToMemento());
+        
+        originator.setState("State 4");
+        System.out.println("Current state: " + originator.getState());
+        
+        originator.getStateFromMemento(careTaker.get(0));
+        System.out.println("First saved state: " + originator.getState());
+        
+        originator.getStateFromMemento(careTaker.get(1));
+        System.out.println("Second saved state: " + originator.getState());
+    }
+}
